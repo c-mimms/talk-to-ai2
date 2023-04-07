0d47371c2d6abad57228af20c475b1b232f9ae0c
Christopher Mimms <cmimms@squareup.com>
2023-04-06 23:23:24 -0400

    Super simplified with browser STT and TTS.

--- index.js
+++ index.js
@@ -1,109 +1,22 @@
-import { spawn, exec } from 'child_process';
-import fs from 'fs';
-import record from 'node-record-lpcm16';
-import 'axios';
-import { streamGpt } from './gpt.js';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { streamGpt } from './gpt.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 var messageHistory = [];
 
-class AudioRecorder {
-    constructor(device) {
-        console.log(device);
-        this.device = device;
-        this.fileName = "audio";
-        this.fileNum = 0;
-        this.file = null;
-        this.recording = null;
-        this.audioStream = null;
-        this.ffmpeg = spawn('ffmpeg', ['-i', '-', '-af', 'silencedetect=n=-20dB:d=1', '-f', 'null', '-']);
-    }
-
-    init() {
-        this.ffmpeg.stderr.on('data', this.handleFFmpegOutput.bind(this));
-        this.detectRecording = record.record({
-            sampleRate: 16000,
-            device: this.device
-        }).stream().pipe(this.ffmpeg.stdin);
-        this.startRecording();
-    }
-
-    startRecording() {
-        this.fileNum++;
-        this.file = fs.createWriteStream(`${this.fileName}${this.fileNum}_${this.device}.wav`, { encoding: 'binary' });
-        this.recording = record.record({
-            sampleRate: 16000,
-            verbose: true,
-            device: this.device
-        });
-        this.audioStream = this.recording.stream();
-        this.audioStream.pipe(this.file);
-    }
-
-    stopRecording() {
-        this.audioStream.unpipe(this.file);
-        this.audioStream.end();
-        this.file.end();
-        this.recording.stop();
-    }
-
-    async onSilenceDetected() {
-        this.stopRecording();
-        this.startRecording();
-        transcribeAudio(`${this.fileName}${this.fileNum - 1}_${this.device}.wav`);
-    }
-
-    handleFFmpegOutput(data) {
-        const lines = data.toString().split('\n');
-        for (const line of lines) {
-            if (line.includes('silence_start')) {
-                this.onSilenceDetected();
-                break;
-            }
-        }
-    }
-}
-
-async function evaluateCommand(command) {
-    return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                reject(error);
-            } else {
-                resolve(stdout.trim());
-            }
-        });
-    });
-}
-
-function stripBrackets(line) {
-    return line.toString().replace(/\[.*\]/g, '').replace(/\n/g, ' ');
-}
-
-async function transcribeAudio(filename) {
-    const command = `whisper "${filename}" --model tiny.en --language English --fp16 False --output_dir ./trash`;
-    const transcription = await evaluateCommand(command);
-    const strippedMessage = stripBrackets(transcription);
-    if (strippedMessage.length > 5) {
-        updateMessageHistory(strippedMessage);
-    }
-}
-
 function updateMessageHistory(message) {
-    let messageEntry = { 'role': 'user', 'content': message };
-    messageHistory.push(messageEntry);
-    sendToChatGPT();
+  let messageEntry = { 'role': 'user', 'content': message };
+  messageHistory.push(messageEntry);
+  sendToChatGPT();
 
-    // Emit the updated messageHistory to all connected clients
-    io.emit('update', messageHistory);
+  // Emit the updated messageHistory to all connected clients
+  io.emit('update', messageHistory);
 }
 
 // Create an array to store the history of responses from queryGpt
@@ -126,10 +39,6 @@ async function sendToChatGPT() {
 const current_date = new Date().toLocaleString();
 const systemMsg = { 'role': 'system', 'content': `You are VoiceGPT, a voice powered assistant. Current date: ${current_date}` };
 
-//Start listening to default audio input
-var devRec = new AudioRecorder("default");
-devRec.init();
-
 // Create an express app
 const app = express();
 app.use(express.static('public'));
@@ -140,28 +49,32 @@ const io = new Server(server);
 
 // Define a route to serve the web page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // Listen for socket.io connections
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    // Send the initial messageHistory and response to the connected client
-    var response = null;
-    socket.emit('update', messageHistory);
+  // Send the initial messageHistory and response to the connected client
+  socket.emit('update', messageHistory);
 
-    // Listen for disconnection
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  // Listen for speech recognition results from the client
+  socket.on('speechResult', (speechResult) => {
+    updateMessageHistory(speechResult);
+  });
 
-    socket.on('changeSystemMsg', (newSystemMsg) => {
-        systemMsg.content = newSystemMsg;
-      });
+  // Listen for disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('changeSystemMsg', (newSystemMsg) => {
+    systemMsg.content = newSystemMsg;
+  });
 });
 
 // Start the server on port 3000
 server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:3000');
 });
